45dda83895115f0ae780f1cc47a6a98bfe156296
Dan <developerdanx@gmail.com>
2022-08-29 11:48:11 +0200

    Initial Commit

---
+++ C++inSwiftUI/C__inSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  C__inSwiftUIApp.swift
+//  C++inSwiftUI
+//
+//  Created by Dan Petre on 29/8/22.
+//
+
+import SwiftUI
+
+@main
+struct C__inSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
