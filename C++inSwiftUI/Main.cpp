accc50542e882136c83c39e53bdfa3bbe537d5c2
Dan <developerdanx@gmail.com>
2022-08-29 12:25:02 +0200

    C++ Interoperability through a Bridging-Header

---
+++ C++inSwiftUI/Main.cpp
@@ -0,0 +1,12 @@
+//
+//  Main.cpp
+//  C++inSwiftUI
+//
+//  Created by Dan Petre on 29/8/22.
+//
+
+#include "Main.h"
+
+const char* test() {
+    return "Hello from C++";
+}
---
+++ C++inSwiftUI/Main.h
@@ -0,0 +1,20 @@
+//
+//  Main.hpp
+//  C++inSwiftUI
+//
+//  Created by Dan Petre on 29/8/22.
+//
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+const char* test();
+
+#ifdef __cplusplus
+}
+
+#endif
+
+
